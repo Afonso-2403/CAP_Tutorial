a396d6ae44b5fbbab4a807cb55329e07dbc7d4b4
Afonso Luz <avsluz@gmail.com>
2022-09-07 15:41:13 +0200

    Created a custom winston logger

---
+++ srv/utilities/my_logger.js
@@ -0,0 +1,20 @@
+const winston = require('winston');
+
+module.exports =  {
+    create_logger: function({
+        level="info", 
+        format=winston.format.combine(winston.format.timestamp(), winston.format.json()),
+        defaultMeta = { service: 'user-service' },
+        transports = [
+            new winston.transports.File({ filename: 'error.log', level: 'error' }),
+            new winston.transports.Console(),
+          ],
+    }){
+        return winston.createLogger({
+            level: level,
+            format: format,
+            defaultMeta: defaultMeta,
+            transports: transports,
+        });        
+    }
+}
