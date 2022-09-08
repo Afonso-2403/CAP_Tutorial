0abb8b3ea11d62681b9ed61f544ad7d880aa3984
Afonso Vieira da Silva Luz <AVIE@equinor.com>
2022-09-08 11:29:22 +0200

    added winston logger logging to read requests

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -1,10 +1,22 @@
 
 const cds = require('@sap/cds')
-
+const logger_creator = require('./utilities/my_logger')
+const custom_winston_logger = logger_creator.create_logger({
+    defaultMeta: { service: 'risk-service' }
+})
 /**
  * Implementation for Risk Management service defined in ./risk-service.cds
  */
 module.exports = cds.service.impl(async function() {
+
+    this.before('READ', 'Risks', (req) => {
+        custom_winston_logger.log({level: 'error', message: 'Just to test logging an error before request: ' + req})
+    });
+
+    this.before('READ', 'Risks', (req) => {
+        custom_winston_logger.info({message: 'Just to test logging upon request: ' + req})
+    });
+
     this.after('READ', 'Risks', risksData => {
         const risks = Array.isArray(risksData) ? risksData : [risksData];
         risks.forEach(risk => {
