832b682cb6cefff0c5439b711b09060f40501e2f
Afonso Luz <avsluz@gmail.com>
2022-09-19 10:29:00 +0200

    Add RiskManager role + test logs

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -1,23 +1,31 @@
 
 const cds = require('@sap/cds')
-const logger_creator = require('./utilities/my_logger')
+const logger_creator = require('./utilities/my_logger');
 const custom_winston_logger = logger_creator.create_logger({
+    level: 'info',
     defaultMeta: { service: 'risk-service' }
-})
+});
+const CDS_LOG = cds.log('risk-service');
+
 /**
  * Implementation for Risk Management service defined in ./risk-service.cds
  */
 module.exports = cds.service.impl(async function() {
 
     this.before('READ', 'Risks', (req) => {
-        custom_winston_logger.log({level: 'error', message: 'Just to test logging an error before request: ' + req})
+        custom_winston_logger.log({level: 'error', message: 'Just to test logging an error before request from winston: ' + req})
+        CDS_LOG.log('from cds built-in logger')
+        CDS_LOG.error('an error from cds built-in logger')
+        console.log('From the console')
     });
 
-    this.before('READ', 'Risks', (req) => {
-        custom_winston_logger.info({message: 'Just to test logging upon request: ' + req})
+
+    this.on('READ', 'Risks', (req) => {
+        CDS_LOG.warn({message: 'Test logging warn upon request from cds built-in logger: ' + req})
+        return cds.run(req.query)
     });
 
-    this.after('READ', 'Risks', risksData => {
+    this.after('READ', 'Risks', (risksData) => {
         const risks = Array.isArray(risksData) ? risksData : [risksData];
         risks.forEach(risk => {
             if (risk.impact >= 100000) {
--- srv/utilities/my_logger.js
+++ srv/utilities/my_logger.js
@@ -6,7 +6,6 @@ module.exports =  {
         format=winston.format.combine(winston.format.timestamp(), winston.format.json()),
         defaultMeta = { service: 'user-service' },
         transports = [
-            new winston.transports.File({ filename: 'error.log', level: 'error' }),
             new winston.transports.Console(),
           ],
     }){
